481f351bbadc8f8fc14de9191a143ee07be5b9ac
marco-ramirezpp <62118447+marco-ramirezpp@users.noreply.github.com>
2022-02-15 10:24:05 -0500

    Mensaje sistema voz en proceso

--- proyecto1/app/public/routes.py
+++ proyecto1/app/public/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash
 from datetime import datetime
 from werkzeug.utils import secure_filename
 from app.models import Concurso, Participante
