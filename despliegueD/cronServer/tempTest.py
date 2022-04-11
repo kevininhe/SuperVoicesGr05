1d7034d576c4b835ef59821414a47c6f54dc1705
Nicolas Poch Llorente <nicpoch10@gmail.com>
2022-04-11 13:58:48 +0000

    cronserver despliegue D funciona S3 sqs y dynamo, falta probar con web server

---
+++ despliegueD/cronServer/tempTest.py
@@ -0,0 +1,58 @@
+import boto3
+import time
+queue_url = ''
+sqs = boto3.client('sqs',aws_access_key_id = '',aws_secret_access_key = '',region_name = 'us-east-1')
+
+response = sqs.list_queues()
+
+print(response['QueueUrls'])
+
+response = sqs.send_message(
+    QueueUrl=queue_url,
+    DelaySeconds=10,
+    MessageAttributes={
+        'Title': {
+            'DataType': 'String',
+            'StringValue': 'The Whistler'
+        },
+        'Author': {
+            'DataType': 'String',
+            'StringValue': 'John Grisham'
+        },
+        'WeeksOn': {
+            'DataType': 'Number',
+            'StringValue': '6'
+        }
+    },
+    MessageBody=(
+        'Information about current NY Times fiction bestseller for '
+        'week of 12/11/2016.'
+    )
+)
+
+print(response['MessageId'])
+
+time.sleep(60)
+
+response = sqs.receive_message(
+    QueueUrl=queue_url,
+    AttributeNames=[
+        'SentTimestamp'
+    ],
+    MaxNumberOfMessages=1,
+    MessageAttributeNames=[
+        'All'
+    ],
+    VisibilityTimeout=0,
+    WaitTimeSeconds=0
+)
+
+message = response['Messages'][0]
+receipt_handle = message['ReceiptHandle']
+
+# Delete received message from queue
+sqs.delete_message(
+    QueueUrl=queue_url,
+    ReceiptHandle=receipt_handle
+)
+print('Received and deleted message: %s' % message)
---
+++ despliegueD/webServer/entrypoint.py
@@ -0,0 +1,9 @@
+
+from app import create_app
+
+#app = create_app()
+
+if __name__=='__main__':
+    from app import create_app
+    app = create_app()
+    app.run(debug=True,use_reloader=False,host="0.0.0.0", port=5000)
