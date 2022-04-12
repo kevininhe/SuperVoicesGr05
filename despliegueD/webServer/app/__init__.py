8b51bb64d4461dc3e8e90bf23b6afbb4f758425c
Ubuntu <ubuntu@ip-172-31-17-9.ec2.internal>
2022-04-12 00:33:38 +0000

    Kevin: Remove App config parameters from __init__.py file and move them to an untracked app/config.py

--- despliegueD/webServer/app/__init__.py
+++ despliegueD/webServer/app/__init__.py
@@ -8,12 +8,8 @@ db = SQLAlchemy()
 def create_app():
 
     app = Flask(__name__)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@172.28.208.1:49154/app_cloud'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object('app.config.Config')
     app.secret_key = 'secret'
-
-
 
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
